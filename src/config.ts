b5fbae8414dee320de386ea6ca9d42f140172021
Vainius <vmykolaitis1@gmail.com>
2022-07-03 19:23:21 +0300

    Added additional configs

--- src/config.ts
+++ src/config.ts
@@ -8,5 +8,8 @@ export default {
         setWarnMessage: 'set_warn_message',
         setBanMessage: 'set_ban_message',
         setMuteMessage: 'set_mute_message',
+        setFailedCaptchaMessage: 'set_captcha_message',
     },
+    captchaTimeout: 3 * 60 * 1000,
+    messageDeleteTiemout: 3 * 60 * 1000,
 };
